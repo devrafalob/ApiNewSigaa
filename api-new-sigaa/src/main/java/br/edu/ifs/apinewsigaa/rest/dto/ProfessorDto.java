6fc50868af9bf4f1b48a93bb720ad325775acdb7
Rafael Lima Lobão <rafael.lobao104@academico.ifs.edu.br>
2024-08-07 20:28:26 -0300

    Commit

--- api-new-sigaa/src/main/java/br/edu/ifs/apinewsigaa/rest/dto/ProfessorDto.java
+++ api-new-sigaa/src/main/java/br/edu/ifs/apinewsigaa/rest/dto/ProfessorDto.java
@@ -9,7 +9,7 @@ import java.util.Date;
 public class ProfessorDto {
     @Column(name = "nome", length = 255, nullable = false)
     private String nome;
-    @Column(name = "celular", length = 14, nullable = false, unique = true)
+    @Column(name = "celular", length = 11, nullable = false, unique = true)
     private String celular;
     @Column(name = "matricula", nullable = false, unique = true)
     private int matricula;
